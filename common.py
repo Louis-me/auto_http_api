dea94183b8cfd606b76e3668717e8f672ad7117a
284772894 <284772894@qq.com>
2016-07-21 20:22:13 +0800

    del

--- common.py
+++
@@ -1,9 +0,0 @@
-__author__ = 'Administrator'
-class Goals(object):
-    URL = ''               # 接收接口url
-    PARAMS = {}            # 接收接口参数
-    RESULT = 'Fail'       # 接收测试结果
-    REALLY_RESULT = ""
-    LOGIN_KEY = ""
-    LOGIN_VALUE = ""
-    HEADER = {'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8','User-Agent':'Mozilla/5.0 (Windows NT 6.1; rv:29.0) Gecko/20100101 Firefox/29.0'}
